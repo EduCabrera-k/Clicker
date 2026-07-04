0813a53ab7e0122f91098c47f4a5b1dc354b5a04
Lalo <al070801@uacam.mx>
2026-07-04 02:52:34 -0600

    Juegi beta: clicker

---
+++ funciones.js
@@ -0,0 +1,72 @@
+const boton = document.querySelector('#Dark');
+const click = document.querySelector('#Clicker');
+const Cuerpo = document.body;
+const audio = document.querySelector('#audio');
+const noti = document.querySelector('.notiOculta');
+const notiaudio = document.querySelector('#audionoti');
+const btnmusica = document.querySelector('#btnmusica');
+let audioNoti = new Audio('noti.m4a');
+let audioReproducido = false;
+
+btnmusica.addEventListener('click' , () =>{
+
+    if(audio.paused){
+        audio.play();
+        btnmusica.innerText = '🔇';
+    }else{
+        audio.pause();
+        btnmusica.innerText = '🔊';
+    }
+})
+
+boton.addEventListener('click', () =>{
+    Cuerpo.classList.toggle('modooscuro');
+
+    if(Cuerpo.classList.contains('modooscuro')){
+        boton.innerText = '☀️';
+    } else {
+        boton.innerText = '🌙'
+    }
+
+})
+
+click.addEventListener('click', () =>{
+    // Reproducir audio en el primer click
+    if(audioReproducido=== false){
+        audio.play();
+        audioReproducido = true;
+    }
+    
+    let contador = document.querySelector('#contador');
+    let valorActual = parseInt(contador.innerText) + 1;
+
+    contador.innerText = valorActual;
+
+    if(valorActual === 30){
+        audioNoti.play();
+        noti.classList.toggle('notishow');
+        noti.innerText = 'Logro Desbloqueado: \n🎉Primeros Clicks 🎉';
+        setTimeout(() => {
+            noti.classList.remove('notishow');
+        }, 5000);
+
+    } if(valorActual === 200){
+        audioNoti.play();
+        noti.classList.toggle('notishow');
+        noti.innerText = 'Logro Desbloqueado: \n🎉 Agarrando Vuelo 🎉';
+        setTimeout(() => {
+            noti.classList.remove('notishow');
+        }, 5000);
+
+    } if(valorActual === 400){
+        audioNoti.play();
+        noti.classList.toggle('notishow');
+        noti.innerText = 'Logro Desbloqueado: \n🎉 Clicker Principiante 🎉';
+        setTimeout(() => {
+            noti.classList.remove('notishow');
+        }, 5000);
+
+    }
+})
+
+
